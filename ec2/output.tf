4ba72847e51b55734eb912646ebab0c90991701e
Maksym Derbenev <dereban25@gmail.com>
2020-08-12 11:45:12 +0300

    Send to git modules of terraform

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,6 +1,6 @@
 output "instance_id" {
   value = aws_instance.ec2-instance.id
 }
-output "public_ip" {
-  value = aws_instance.public_ip
-}
+//output "public_ip" {
+//  value = aws_instance.associate_public_ip_address
+//}
